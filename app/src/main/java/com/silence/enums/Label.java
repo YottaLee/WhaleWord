57190f48c3ecfe95bd43f12b82b8a9df9fce478c
PanxyCris <739312347@qq.com>
2019-06-22 11:38:57 +0800

    word label

---
+++ app/src/main/java/com/silence/enums/Label.java
@@ -0,0 +1,7 @@
+package com.silence.enums;
+
+public enum Label {
+    Handled, //已掌握
+    Unfamiliar, //生词
+    Studied //已学词
+}
--- app/src/main/java/com/silence/pojo/newword.java
+++ app/src/main/java/com/silence/pojo/newword.java
@@ -2,20 +2,49 @@ package com.silence.pojo;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import com.silence.enums.Label;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class newword  implements Parcelable {
+public class newword implements Parcelable {
 
     private int mid;
     private String mkey;
     private List<Trans> mtranslist;
+    private List<Label> labels;
 
     public newword(int mid, String mkey, List<Trans> mtranslist) {
         this.mid = mid;
         this.mkey = mkey;
         this.mtranslist = mtranslist;
+        labels = new ArrayList<>();
+    }
+
+    /**
+     * 掌握该单词
+     */
+    public void handle(){
+        if(!labels.contains(Label.Studied))
+            labels.add(Label.Studied);
+        if(!labels.contains(Label.Handled)) {
+            if(labels.contains(Label.Unfamiliar))
+                labels.remove(Label.Unfamiliar);
+            labels.add(Label.Handled);
+        }
+    }
+
+    /**
+     * 忘记该单词
+     */
+    public void forget(){
+        if(!labels.contains(Label.Studied))
+            labels.add(Label.Studied);
+        if(!labels.contains(Label.Unfamiliar)) {
+            if(labels.contains(Label.Handled))
+                labels.remove(Label.Handled);
+            labels.add(Label.Unfamiliar);
+        }
     }
 
     public int getMid() {
