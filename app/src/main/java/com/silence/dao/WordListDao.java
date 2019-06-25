b5e74808bfe0bf11ea7901bfe71e7aa8d71638ed
2529716798 <2529716798@qq.com>
2019-06-25 15:55:46 +0800

    init json file on sd

---
+++ app/src/main/java/com/silence/dao/WordListDao.java
@@ -0,0 +1,9 @@
+package com.silence.dao;
+
+/**
+ * @Author: py
+ * @Date: 2019-06-25 15:34
+ * @Version 1.0
+ */
+public class WordListDao {
+}
---
+++ app/src/main/java/com/silence/utils/GsonUtil.java
@@ -0,0 +1,9 @@
+package com.silence.utils;
+
+/**
+ * @Author: py
+ * @Date: 2019-06-25 13:21
+ * @Version 1.0
+ */
+public class GsonUtil {
+}
